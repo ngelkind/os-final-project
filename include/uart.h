2397482a074239796653cc57c878f3bd86b5f780
Nachum Getzel Elkind <95549660+ngelkind@users.noreply.github.com>
2026-08-31 10:38:21 +0300

    char representation of user pressed keys replaced with enum class
    printing extended with function println with default "" param passed for functionality close to high level print implementation

--- include/uart.h
+++ include/uart.h
@@ -4,7 +4,14 @@ void print_symbol(char ch);
 extern "C" void kernel_main();
 void print_string (const char* string);
 void print_hex (uint64_t value);
-char decimal_to_hex (const int value);
+char decimal_to_hex (int value);
 char read_symbol ();
 int read_input(char* buffer, int max_length);
 bool are_string_equals(const char* f, const char* s);
+void println (const char* string = "");
+enum class KEYS : char {
+    BACKSPACE_FIRST = 8,
+    BACKSPACE_SECOND = 127,
+    ENTER = 13,
+
+};
--- src/uart.cpp
+++ src/uart.cpp
@@ -11,21 +11,21 @@ int read_input(char* buffer, const int max_length) {
     int i = 0;
     while (true) {
         const char c = read_symbol();
-        if (c == 8 || c == 127) { //backspace
+        if (c == static_cast<char>(KEYS::BACKSPACE_FIRST) || c == static_cast<char>(KEYS::BACKSPACE_SECOND)) {
             if (i > 0) {
-                print_string("\b \b");
+                print_string("\b \b"); //b moves cursor to one pos left, space override char with emptiness, backspace going left after the space moved the cursor again
                 --i;
             }
             continue;
         }
-        if (c == '\r') { //enter
-            print_string("\n");
+        if (c == static_cast<char>(KEYS::ENTER)) {
+            println();
             break;
         }
         if (i == max_length - 1) {
-            while (read_symbol() != '\r') {
+            while (read_symbol() != static_cast<char>(KEYS::ENTER)) {
             } // let user finish the command and only then throw an error
-            print_string("\n");
+            println();
             buffer[i] = 0;
             return -1;
         }
@@ -53,15 +53,19 @@ void print_string(const char* string) {
         if (string[i] != '\n') {
             print_symbol(string[i]);
         } else {
-            print_symbol('\r'); // r for move the cursor to the beg of the line
+            print_symbol('\r'); // r for moving the cursor to the beg of the line
             print_symbol('\n');
         }
     }
 }
+void println (const char* string) {
+    print_string(string);
+    print_string("\n");
+}
 void print_hex(uint64_t value) {
-    char values[16] = {0};
-
+    char values[16];
     int i = 0;
+
     do {
         values[i++] = decimal_to_hex(static_cast<int>(value) % 16);
         value /= 16;
